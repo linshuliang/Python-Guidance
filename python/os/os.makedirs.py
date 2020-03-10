fe6d5cdbebfa99034d46f861c755ca84907c18f3
David Lin <xiaolin_2292@qq.com>
2020-03-10 16:30:19 +0800

    Update os.makedirs.py

--- python/os/os.makedirs.py
+++ python/os/os.makedirs.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # os.makedirs() 方法用于递归创建目录，
 # 但创建的所有 intermediate-level 文件夹需要包含子目录。
-# 语法：os.makedirs(path, mode=0777)
+# 语法：os.makedirs(path, mode=0o777)
 import os
 
 cwd = os.getcwd()
