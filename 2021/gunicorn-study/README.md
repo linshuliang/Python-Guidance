dcea9b8fd661f1f47077a96dd2244941e8f7c98e
linshuliang <xiaolin_2292@qq.com>
2021-06-08 20:57:30 +0800

    gunicorn

---
+++ 2021/gunicorn-study/README.md
@@ -0,0 +1,17 @@
+# gunicorn
+
+## 简介
+
+![green unicorn logo](./gunicorn_logo.jpg)
+
+[Gunicorn](https://docs.gunicorn.org/en/stable/index.html) 全称是 Green unicorn，它是一个适用于 UNIX 的 Python WSGI HTTP 服务器。
+Gunicorn 服务器与各种 web 框架广泛兼容，实现简单，服务资源少，速度相当快。
+
+特性：
+
+* 原生支持 `WSGI`, `Django`, `Paster`
+* 自动管理工作进程
+* 简单的 Python 配置
+* 多个工作器配置
+* 可拓展的各种服务挂钩(Sever hook)
+* 兼容 Python 3.5 以上版本
---
+++ 2021/gunicorn-study/demo_1/readline_app.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from gunicorn import __version__
+
+
+def app(environ, start_response):
+    """Simplest possible application object"""
+    status = '200 OK'
+
+    response_headers = [('Content-type', 'text/plain'), ('Transfer-Encoding', "chunked"),
+                        ('X-Gunicorn-Version', __version__)]
+    start_response(status, response_headers)
+
+    body = environ['wsgi.input']
+
+    lines = []
+    while True:
+        line = body.readline()
+        if line == b"":
+            break
+        print(line)
+        lines.append(line)
+
+    return iter(lines)
